33891ce39f4a14053e797fafd46d34568257fee1
Sara Pranjić <sp50165@fer.hr>
2020-04-09 19:33:58 +0200

    add playerball and camera instantiating

--- MultiplayerBall/Assets/Mutliplayer Assets/Scripts/SingleUse/SimpleObjectMover.cs
+++ MultiplayerBall/Assets/Mutliplayer Assets/Scripts/SingleUse/SimpleObjectMover.cs
@@ -8,25 +8,25 @@ public class SimpleObjectMover : MonoBehaviourPun, IPunObservable
 
     [SerializeField]
     private float _moveSpeed = 1f;
-    private Animator _animator;
+    //private Animator _animator;
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-            /*if (stream.IsWriting) //owner
-            {
-                stream.SendNext(transform.position);
-                stream.SendNext(transform.rotation);
-            }
-            else if (stream.IsReading) //client
-            {
-                transform.position = (Vector3)stream.ReceiveNext();
-                transform.rotation = (Quaternion)stream.ReceiveNext();
-            }*/
+        /*if (stream.IsWriting) //owner
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else if (stream.IsReading) //client
+        {
+            transform.position = (Vector3)stream.ReceiveNext();
+            transform.rotation = (Quaternion)stream.ReceiveNext();
+        }*/
     }
 
     private void Awake()
     {
-        _animator = GetComponent<Animator>();
+     //   _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -34,14 +34,14 @@ public class SimpleObjectMover : MonoBehaviourPun, IPunObservable
         if (base.photonView.IsMine)
         {
             float x = Input.GetAxisRaw("Horizontal");
-            float y = Input.GetAxisRaw("Vertical");
-            transform.position += (new Vector3(x, y, 0f) * _moveSpeed);
-            UpdateMovingBoolean(x != 0f || y != 0f);
+            float z = Input.GetAxisRaw("Vertical");
+            transform.position += (new Vector3(x, 0f, z) * _moveSpeed);
+           // UpdateMovingBoolean(x != 0f || y != 0f);
         }
     }
 
     private void UpdateMovingBoolean(bool moving)
     {
-        _animator.SetBool("moving", moving);
+       // _animator.SetBool("moving", moving);
     }
 }
